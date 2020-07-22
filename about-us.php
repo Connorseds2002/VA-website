8239c8d1ee7900284e24dea048dbc07375966725
Connorseds2002 <54939589+Connorseds2002@users.noreply.github.com>
2020-07-22 22:29:22 +0100

    Create about-us.php

---
+++ about-us.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/website.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>About us vEuroJet</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+
+ <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="stylesheet/layout.css">
+  <body>
+
+    <!-- Navigation -->
+  <nav class="w3-bar w3-black">
+	  <a href="index.php"><img src="./images/logo.png" width="10%"></a>
+	  <p></p>
+      <a href="./index.php" class="w3-button w3-bar-item">Home</a>
+      <a href="./about-us.php" class="w3-button w3-bar-item">Info</a>
+      <a href="./logon.php" class="w3-button w3-bar-item">Logon</a>
+      <a href="./contact.php" class="w3-button w3-bar-item">Contact</a>
+	  <a href="./signup.php" class="w3-button w3-bar-item">Signup</a>
+    </nav>
+    <!-- InstanceBeginEditable name="EditRegion1" --><h1>About Us Page</h1>
+        <p>We are a Virtal Airline based arround the charter airline called EuroJet </p>
+        <p>As a Fectunal Virtal Airline We have a set route when you can fly the aircraft that is in out fleet </p>
+        <p>Virtual EuroJer aims to offer stunning freedom and opportunities for 'flight-sim' enthusiasts across Europe and the world.</p>
+    <p>We let you fly anywhere, yes anywhere, you wish, following two simple rules:</p>
+    <blockquote>
+      <p>- Flying to where we have a route to</p>
+    </blockquote>
+    <blockquote>
+      <p>- Making shore that the aircraft that are beening flown shoe be correact then a A319 should not be flowen from EGCC-TNCM as it would excide that max flight range.</p>
+    </blockquote>
+</div>
+      
+      <h2 style="text-align:center">Our Team</h2>
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <img src="./images/team1.png" alt="CEO" width="12%">
+            <div class="container">
+              <h2>Kaiden Welsby - EJV008</h2>
+              <p class="title">CEO</p>
+              <p>In cotrol of the VA and desited what is going to hapen with in the va and desised who should be added to a staff team.</p>
+				<p>In control in getting the VA more member by promating the VA</p>
+              <p><a href="mailto:Kaiden.welsby@vEuroJet.uk"> Kaiden.welsby@vEuroJet.uk </a></p>
+            </div>
+          </div>
+        </div>
+      
+        <div class="column">
+          <div class="card">
+            <img src="/w3images/team2.jpg" alt="Louie" width="12%">
+            <div class="container">
+              <h2>Louie Hatch - EJV0002</h2>
+              <p class="title">Director / Events orgniser</p>
+              <p>Accesent to the CEO of the VA. </p>
+				<p>Mananges of the events that hapen with in the VA and send masanger to the server about new events</p>
+              <p><a href="mailto:Louie.Hatch@vEuroJet.uk"> Louie.Hatch@vEuroJet.uk </a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <img src="./images/team5.png" alt="Connor" width="12%">
+            <div class="container">
+              <h2>Connor Sedman - EJV0007</h2>
+              <p class="title">Website Designer / Operation Lead</p>
+              <p>I am the website Coder/Designer for this Virtal Airline and i alos mananger a team under me that deal with the operations part of he VA and reports back to the CEO</p>
+              <p><a href="mailto:connor.sedman@justicemc.co.uk"> connor.sedman@justicemc.co.uk </a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+              <img src="./images/team6.png" alt="Alex" width="12%">
+              <div class="container">
+                <h2>Adam Warren - EJV0008</h2>
+                <p class="title">Discord Bot Coder/manager</p>
+                <p>Creates the discord bot and mananger the bots on the server with in reasion from the CEO of the VA</p>
+                <p><a href="mailto:Adam.Warren@vEuroJet.uk"> Adam.Warren@vEuroJet.uk </a></p>
+              </div>
+            </div>
+          </div>
+		  <div class="column">
+			  <div class="card">
+				  <img src="images/Image7.png" alt="Balash" width="12%">
+				  <div class="container">
+					  <h2>Balash - EJV0010</h2>
+					  <p class="title">Accesentient to the Director</p>
+					  <p>An accesent to the Director on the VA compleates taskes that have ben asined from the Director</p>
+					  <p><a href="mailto:Balash@vEuroJet.uk">Blash@vEuroJet.uk</a></p>
+				  </div>
+			  </div>
+		  </div>
+<!-- InstanceEndEditable -->
+
+	  <footer class="w3-container w3-padding-64 w3-center w3-blue-grey w3-xlarge">
+      <a href="#"><i class="fa fa-facebook-official"></i></a>
+      <a href="#"><i class="fa fa-twitter"></i></a>
+      <p class="w3-medium">
+        This website has been crated by <a href="https://wwww.justicemc.co.uk/info" target="_blank">Connnor Sedman - EJV007</a>
+      </p>
+</footer>
+</body>
+<!-- InstanceEnd --></html>
