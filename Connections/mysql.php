3980fc5f5f403c39796e0c288a78c5b9f942769d
Connorseds2002 <54939589+Connorseds2002@users.noreply.github.com>
2020-07-23 15:16:39 +0100

    Update mysql.php

--- Connections/mysql.php
+++ Connections/mysql.php
@@ -6,5 +6,5 @@ $hostname_mysql = "localhost";
 $database_mysql = "va";
 $username_mysql = "root";
 $password_mysql = "connorcC@12";
-$mysql = mysqli_connect($hostname_mysql, $username_mysql, $password_mysql)
+$mysql = mysqli_connect($hostname_mysql, $username_mysql, $password_mysql, $database_sysql)
 ?>
